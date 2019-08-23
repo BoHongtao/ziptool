e6ad5ecc6c999da5562b55d42770417b7ec2ea7d
boht@chocwell.com <BoHongtao@yeah.net>
2019-08-23 11:28:58 +0800

    zip_tool

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ziptool
-compress you picture
+压缩你的图片，调用了tinypng的接口，本脚本对其进行了封装
+
+# use
+确保安装了python3环境
+ 
+1. pip install tinify
+2. python zip.py
+
---
+++ zip.py
@@ -0,0 +1,27 @@
+import tinify
+import os
+
+tinify.key = 'qdlt2hYBDKsZWKRTNjKQv1VhYxs3r73x'
+
+fileArray = []
+
+# 递归深度遍历文件
+def traverse(f):
+    fs = os.listdir(f)
+    for f1 in fs:
+        tmp_path = os.path.join(f, f1)
+        if not os.path.isdir(tmp_path):
+            if is_img(tmp_path):
+                fileArray.append(tmp_path)
+        else:
+            traverse(tmp_path)
+
+# 根据路径判断文件是不是img
+def is_img(path):
+    path = path.split('.')[-1].upper()
+    return True if (path == 'PNG') | (path == 'JPG') | (path=='JPEG') else False
+
+traverse('E:\codeingSpace\zipimg\img')
+for imgpath in fileArray:
+    print("compressing ..." + imgpath)
+    tinify.from_file(imgpath).to_file(imgpath)
